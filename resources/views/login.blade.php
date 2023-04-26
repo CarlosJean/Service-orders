46f32728ef0648b5a772525a6b72e648abd94804
Jean Holguin <holguinjean1@gmail.com>
2023-04-25 22:43:23 -0400

    Login controller created.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<form action="dashboard.php">
+    <div class="form-group">
+        <label>Correo</label>
+        <input type="text" class="form-control" placeholder="Ingrese su correo" value="demo@bootstrapdash.com">
+    </div><!-- form-group -->
+    <div class="form-group">
+        <label>Contraseña</label>
+        <input type="password" class="form-control" placeholder="Ingrese su contraseña" value="thisisademo">
+    </div><!-- form-group -->
+    <button class="btn btn-az-primary btn-block">Iniciar Sesión</button>
+</form>
