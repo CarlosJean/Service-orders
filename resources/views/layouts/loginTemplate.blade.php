96085ee63310eafb951ded76877cb7953541cc8a
Jean Holguin <holguinjean1@gmail.com>
2023-04-26 20:39:33 -0400

    Login layout created. Login view created.

---
+++ resources/views/layouts/loginTemplate.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>
+        Sistema de ordenes de servicio | @yield('title','Acceso')
+    </title>
+
+
+    @vite(['resources/sass/app.scss','resources/css/azia.min.css'])
+    
+</head>
+
+<body class="az-body">
+    @yield('content')
+</body>
+
+</html>
