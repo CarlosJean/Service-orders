cebfb1115ab6cbc21989a021b3f7a83cb361b8d1
Jean Holguin <holguinjean1@gmail.com>
2023-05-01 10:12:09 -0400

    Submenus by role tables created
    *Role, menu and submenu models created.
    *Menu and submenu seeders created.
    *Menu and submenu migrations created.

---
+++ database/seeders/SubmenusTableSeeder.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SubmenusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Menú de personas
+        DB::table('submenus')->insert([
+            'name' => 'Tecnicos',
+            'order' => 1,
+            'menu_id' => 2
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Usuarios',
+            'order' => 2,
+            'menu_id' => 2
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Roles',
+            'order' => 3,
+            'menu_id' => 2
+        ]);
+        
+        //Menú de compras
+        DB::table('submenus')->insert([
+            'name' => 'Órdenes de compra',
+            'order' => 1,
+            'menu_id' => 3
+        ]);
+
+        DB::table('submenus')->insert([
+            'name' => 'Cotizaciones',
+            'order' => 2,
+            'menu_id' => 3
+        ]);
+
+        DB::table('submenus')->insert([
+            'name' => 'Suplidor',
+            'order' => 3,
+            'menu_id' => 3
+        ]);
+        
+        //Menú de inventarios
+        DB::table('submenus')->insert([
+            'name' => 'Artículos',
+            'order' => 1,
+            'menu_id' => 4
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Valor de inventario',
+            'order' => 2,
+            'menu_id' => 4
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Ajustes de inventario',
+            'order' => 3,
+            'menu_id' => 4
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Categorías',
+            'order' => 4,
+            'menu_id' => 4
+        ]);
+        
+        //Menú de órdenes
+        DB::table('submenus')->insert([
+            'name' => 'Servicios',
+            'order' => 1,
+            'menu_id' => 5
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Departamentos',
+            'order' => 2,
+            'menu_id' => 5
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Órdenes de servicios',
+            'order' => 3,
+            'menu_id' => 5
+        ]);
+        
+        DB::table('submenus')->insert([
+            'name' => 'Gestión de materiales',
+            'order' => 4,
+            'menu_id' => 5
+        ]);
+        
+        //Menú de reportes
+        DB::table('submenus')->insert([
+            'name' => 'Reportes',
+            'order' => 1,
+            'menu_id' => 6
+        ]);
+
+    }
+}
