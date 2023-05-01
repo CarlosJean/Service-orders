89e2482bc351e63ec46817df03179dd7523fd53e
Jean Holguin <holguinjean1@gmail.com>
2023-05-01 00:31:12 -0400

    Menus on navbar by role added.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,16 @@
 @foreach($roleMenus as $menu)
-    <li class="nav-item">
-        <a href="#" class="nav-link">
-            {{$menu}}
-        </a>
-    </li>
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        <i class="{{$menu['menu']['icon']}}"></i>
+        {{$menu['menu']['name']}}
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+        @foreach($menu['submenus'] as $submenu)
+            <a class="dropdown-item" href="{{ route('logout') }}">
+                {{$submenu['name']}}
+            </a>
+        @endforeach
+    </div>
+</li>
 @endforeach
