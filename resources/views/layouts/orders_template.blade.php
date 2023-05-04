ea6be3ecb8d479798b95af389296bfee8b49fedf
Jean Holguin <holguinjean1@gmail.com>
2023-05-03 21:45:35 -0400

    Orders template created.
    *Orders template created. Orders views will share an image of the company,
    name of order and number so we created a template.
    *An example of order template implementation also created,
    through orders.blade.php

--- resources/views/layouts/orders_template.blade.php
+++ resources/views/layouts/orders_template.blade.php
@@ -1,13 +1,43 @@
 @extends('layouts.app')
 
+@section('content')
 <div class="container">
     <div class="row">
-        <div class="col-6">
-            @section('content')
-                <div class="card shadow">
-                    @yield('orderContent')
+        <div class="col-11">
+            <div class="card shadow">
+                <div class="row py-5 px-5 justify-content-between">
+                    <div class="col-3">
+                        <img src="{{url('/drl_manufacturing.png')}}" alt="DRL Manufacturing logo" srcset="" width="100%">
+                    </div>
+                    <div class="col-4">
+                        <div class="row">
+                            <!-- Nombre de la orden -->
+                            <div class="col-12 text-end">
+                                <h3>@yield('screenName', 'Órdenes de...')</h3>
+                            </div>
+                            <div class="col-12">
+                                <div class="row align-items-center">
+                                    <div class="col-2">
+                                        <p class="my-0">No.</p>
+                                    </div>
+                                    <div class="col-10">
+                                        <!-- Input con el número de orden -->
+                                        @stack('orderNumber')
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            @endsection
+                <div class="row">
+                    <div class="col-12 px-4">
+                        <hr class="mt-3 opacity-100" />
+                    </div>
+                    <div class="col-12 px-5">
+                        @yield('orderContent')
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    @endsection
--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.orders_template')
 
+@section('screenName', 'Orden de servicios')
+@push('orderNumber')
+<input type="text" name="" id="" value="0" class="form-control text-end" readonly>
+@endpush
+
 @section('orderContent')
     <h1>Prueba</h1>
 @endsection
