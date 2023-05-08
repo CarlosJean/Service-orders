621d620c04e2c3e7eb32bf886b30def959e4af4e
Jean Holguin <holguinjean1@gmail.com>
2023-05-07 22:56:23 -0400

    Datatables installed.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
 import './bootstrap';
 import "typicons.font";
+
+import jquery from 'jquery';
+window.$ = jquery;
+
+import DataTables from 'datatables.net-bs5';
+import 'datatables.net-responsive-bs5';
