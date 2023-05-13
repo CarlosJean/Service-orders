2fb9c65886b238dc2a961d9cf9fb7d000accc99c
Xavier Vasquez <xavier_v89]@hotmail.com>
2023-05-13 15:49:50 -0400

    agregando vista nuevas

---
+++ resources/views/GestionMateriales.blade.php
@@ -0,0 +1,31 @@
+
+@extends('layouts.app')
+
+
+@section('content')
+
+
+@vite(['resources/js/orders.js'])
+
+
+<div class="container">
+      <a class="btn btn-primary float-right" href="/service-orders/public/GestionMaterialesBTN" >+ Nueva solicitud de materiales</a>
+    <br>   
+    <br> 
+      <table id="example" class="table table-striped table-bordered nowrap" style="font-size:13px">
+        <thead>
+            <tr>
+                <th>Numero</th>
+                <th>Nombre</th>
+                <th>Fecha</th>
+                <th>Departamento</th>
+                <th>Servicio</th>
+                <th>Estado</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+       
+    </table>
+@endsection
+
+
--- resources/views/ordersSupGen.blade.php
+++ resources/views/ordersSupGen.blade.php
@@ -9,7 +9,7 @@
 
 
 <div class="container">
-      <a class="btn btn-primary float-right" href="/service-orders/public/ordersSup" >+ Nueva orden de servicio </a>
+      <a class="btn btn-primary float-right" href="/service-orders/public/ordersSupGenRen" >+ Nueva orden de servicio </a>
       <br>
     <br>
       <table id="example" class="table table-striped table-bordered nowrap" style="font-size:13px">
