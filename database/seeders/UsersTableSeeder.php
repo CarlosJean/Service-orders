16682dc9cd95a441496944ce4465cb3786c8b6ce
Jean Holguin <holguinjean1@gmail.com>
2023-05-18 22:41:25 -0400

    Services added to Xavier as technician

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,20 +16,20 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert(
             [
-                'name' => 'holguinjean1@gmail.com',
+                'name' => 'Jean Holguin',
                 'email' => 'holguinjean1@gmail.com',
                 'password' => Hash::make('holguinjean1@gmail.com'),
             ]
         );
         DB::table('users')->insert(
             [
-                'name' => 'jhosuabrown@gmail.com',
+                'name' => 'Jhosua Brown',
                 'email' => 'jhosuabrown@gmail.com',
                 'password' => Hash::make('jhosuabrown@gmail.com'),
             ]
         );        
         DB::table('users')->insert([
-            'name' => 'yoel@gmail.com',
+            'name' => 'Yoel',
             'email' => 'yoel@gmail.com',
             'password' => Hash::make('yoel@gmail.com'),
         ]);
