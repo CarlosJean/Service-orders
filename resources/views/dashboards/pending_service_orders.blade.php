ea4036561bf122477cfe3f92f115e429a81464f4
Jean Holguin <holguinjean1@gmail.com>
2023-09-09 16:01:06 -0400

    Service orders details button on dashboard

--- resources/views/dashboards/pending_service_orders.blade.php
+++ resources/views/dashboards/pending_service_orders.blade.php
@@ -5,6 +5,7 @@
             <th>Nombre</th>
             <th>Fecha</th>
             <th>Estado</th>
+            <th>Detalles</th>
         </tr>
     </thead>
     <tbody>
@@ -14,6 +15,11 @@
             <td>{{$serviceOrder->name}}</td>
             <td>{{$serviceOrder->created_at}}</td>
             <td>{{$serviceOrder->status}}</td>
+            <td>
+                <a href="ordenes-servicio/{{$serviceOrder->number}}" class="btn btn-secondary">
+                    Ver
+                </a>
+            </td>
         </tr>
         @endforeach
     </tbody>
